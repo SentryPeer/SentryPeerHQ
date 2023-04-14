ebc24fb08ff78be780611ae168346e93dd51d292
Gavin Henry <ghenry@suretec.co.uk>
2023-04-14 18:56:36 +0100

    Remove SVG fade background.

--- lib/sentrypeer_web/components/home_page_components.ex
+++ lib/sentrypeer_web/components/home_page_components.ex
@@ -20,58 +20,7 @@ defmodule SentrypeerWeb.HomePageComponents do
   def home_page_header(assigns) do
     ~H"""
     <header>
-      <div class="relative isolate bg-white dark:bg-slate-800">
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-          <svg
-            class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-            viewBox="0 0 1155 678"
-          >
-            <path
-              fill="url(#9b2541ea-d39d-499b-bd42-aeea3e93f5ff)"
-              fill-opacity=".3"
-              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-            />
-            <defs>
-              <linearGradient
-                id="9b2541ea-d39d-499b-bd42-aeea3e93f5ff"
-                x1="1155.49"
-                x2="-78.208"
-                y1=".177"
-                y2="474.645"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stop-color="#9089FC" />
-                <stop offset="1" stop-color="#6D50A0" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>
-        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-          <svg
-            class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-            viewBox="0 0 1155 678"
-          >
-            <path
-              fill="url(#b9e4a85f-ccd5-4151-8e84-ab55c66e5aa1)"
-              fill-opacity=".3"
-              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-            />
-            <defs>
-              <linearGradient
-                id="b9e4a85f-ccd5-4151-8e84-ab55c66e5aa1"
-                x1="1155.49"
-                x2="-78.208"
-                y1=".177"
-                y2="474.645"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stop-color="#9089FC" />
-                <stop offset="1" stop-color="#6D50A0" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>
-
+      <div class="relative bg-white dark:bg-slate-800">
         <div class="mx-auto flex max-w-7xl items-center justify-between p-6 md:justify-start md:space-x-10 lg:px-8">
           <div class="flex justify-start lg:w-0 lg:flex-1">
             <a href="/" title="SentryPeer Home">
