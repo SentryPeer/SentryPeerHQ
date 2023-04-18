44388e1c614cae0e932174036993eecbda5728e2
Gavin Henry <ghenry@suretec.co.uk>
2023-04-18 22:48:30 +0100

    Back to top of page button and JS - https://tailwind-elements.com/snippets/tailwind/tailwindelements/3681385?view=side#js-tab-view and spacing fixes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,3 +89,29 @@ window.addEventListener("toogle-darkmode", e => {
 
 initDarkMode();
 
+// GScroll To Top
+let scrollToTopButton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
--- lib/sentrypeer_web/components/home_page_components.ex
+++ lib/sentrypeer_web/components/home_page_components.ex
@@ -295,6 +295,28 @@ defmodule SentrypeerWeb.HomePageComponents do
 
   def footer(assigns) do
     ~H"""
+    <!-- Back to top button -->
+    <button
+      type="button"
+      title="Back to top of page"
+      class="float-right inline-block p-3 bg-brand text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-indigo-600 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out bottom-5 right-5"
+      id="btn-back-to-top"
+    >
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        class="w-4 h-4"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path
+          fill="currentColor"
+          d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
+        >
+        </path>
+      </svg>
+    </button>
     <footer class="bg-gray-50 dark:bg-slate-600" aria-labelledby="footer-heading">
       <h2 id="footer-heading" class="sr-only">Footer</h2>
       <div class="mx-auto max-w-7xl px-6 pt-16 pb-8 lg:px-8 lg:pt-24">
@@ -550,16 +572,27 @@ defmodule SentrypeerWeb.HomePageComponents do
           </div>
           <p class="mt-8 text-sm text-gray-400 dark:text-black md:order-1 md:mt-0">
             ©
-            <a href="https://antnetworks.com/" title="Ant Networks Limited" target="_blank">
+            <a
+              href="https://antnetworks.com/"
+              class="hover:text-brand"
+              title="Ant Networks Limited"
+              target="_blank"
+            >
               Ant Networks Ltd.
             </a>
             <%= DateTime.utc_now().year %>. SentryPeer is a trading name of
-            <a href="https://antnetworks.com/" title="Ant Networks Limited" target="_blank">
+            <a
+              href="https://antnetworks.com/"
+              class="hover:text-brand"
+              title="Ant Networks Limited"
+              target="_blank"
+            >
               Ant Networks Ltd.
             </a>
             The
             <a
               href="https://trademarks.ipo.gov.uk/ipo-tmcase/page/Results/1/UK00003847726"
+              class="hover:text-brand"
               title="The SentryPeer icon is a registered trademark"
               target="_blank"
             >
@@ -568,13 +601,19 @@ defmodule SentrypeerWeb.HomePageComponents do
             and the
             <a
               href="https://trademarks.ipo.gov.uk/ipo-tmcase/page/Results/1/UK00003700947"
+              class="hover:text-brand"
               title="SentryPeer is a registered trademark"
               target="_blank"
             >
               SentryPeer name
             </a>
             are registered trademarks of
-            <a href="https://ghenry.co.uk/about/" title="About Gavin Henry" target="_blank">
+            <a
+              href="https://ghenry.co.uk/about/"
+              class="hover:text-brand"
+              title="About Gavin Henry"
+              target="_blank"
+            >
               Gavin Henry.
             </a>
             All rights reserved.
