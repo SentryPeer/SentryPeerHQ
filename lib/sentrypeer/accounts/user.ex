9ffdbd00628e1a1f318858683781e90a03121d5c
Gavin Henry <ghenry@suretecsystems.com>
2023-05-24 14:11:17 +0100

    Copyright on all .ex files and FunWithFlags.Actor in (almost).

--- lib/sentrypeer/accounts/user.ex
+++ lib/sentrypeer/accounts/user.ex
@@ -1,3 +1,22 @@
+# SPDX-License-Identifier: AGPL-3.0
+# Copyright (c) 2023 Gavin Henry <ghenry@sentrypeer.org>
+#
+#   _____            _              _____
+#  / ____|          | |            |  __ \
+# | (___   ___ _ __ | |_ _ __ _   _| |__) |__  ___ _ __
+#  \___ \ / _ \ '_ \| __| '__| | | |  ___/ _ \/ _ \ '__|
+#  ____) |  __/ | | | |_| |  | |_| | |  |  __/  __/ |
+# |_____/ \___|_| |_|\__|_|   \__, |_|   \___|\___|_|
+#                              __/ |
+#                             |___/
+#
+
+defimpl FunWithFlags.Actor, for: Sentrypeer.Accounts.User do
+  def id(%{auth_id: auth_id}) do
+    "user:#{auth_id}"
+  end
+end
+
 defmodule Sentrypeer.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
