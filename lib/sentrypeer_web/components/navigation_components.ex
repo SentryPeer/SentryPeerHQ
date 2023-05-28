1469f47261477a1a64d67ebe68b2eca52211be8d
Gavin Henry <ghenry@suretec.co.uk>
2023-05-28 21:07:44 +0100

    More explaination on About page and switch to blue for alert notice about plans.

--- lib/sentrypeer_web/components/navigation_components.ex
+++ lib/sentrypeer_web/components/navigation_components.ex
@@ -323,27 +323,27 @@ defmodule SentrypeerWeb.NavigationComponents do
   def in_app_menu(assigns) do
     ~H"""
     <%= if FunWithFlags.enabled?(:tester_plan, for: @current_user) do %>
-      <div class="border-l-4 border-yellow-400 dark:border-slate-600 bg-yellow-50 dark:bg-slate-400 p-4 p-4">
+      <div class="border-l-4 border-blue-400 rounded-md dark:border-slate-600 bg-blue-50 dark:bg-slate-400 p-4 p-4">
         <div class="flex">
           <div class="flex-shrink-0">
             <svg
-              class="h-5 w-5 text-yellow-400 dark:text-slate-800"
+              class="h-5 w-5 text-blue-400 dark:text-slate-800"
               viewBox="0 0 20 20"
               fill="currentColor"
               aria-hidden="true"
             >
               <path
                 fill-rule="evenodd"
-                d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
                 clip-rule="evenodd"
               />
             </svg>
           </div>
           <div class="ml-3">
-            <p class="text-sm text-yellow-700 dark:text-slate-800">
+            <p class="text-sm text-blue-700 dark:text-slate-800">
               You are on the Tester plan.
               <.link
-                class="font-medium text-yellow-700 dark:text-slate-800 underline hover:text-yellow-600 dark:hover:text-slate-600"
+                class="font-medium text-blue-700 dark:text-slate-800 underline hover:text-blue-600 dark:hover:text-slate-600"
                 navigate={~p"/billing"}
                 title="Switch to a paid plan using a credit card via 'Manage Plan'."
               >
