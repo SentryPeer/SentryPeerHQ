d992c7083ac6386fcae989cb01bc15bc3674670b
Gavin Henry <ghenry@suretec.co.uk>
2023-06-19 12:59:16 +0100

    Ecto.Query versions for Analytics general.

--- lib/sentrypeer/analytics.ex
+++ lib/sentrypeer/analytics.ex
@@ -16,45 +16,83 @@ defmodule Sentrypeer.Analytics do
   The Analytics General context.
   """
 
+  import Ecto.Query
   alias Sentrypeer.Repo
+  alias Sentrypeer.SentrypeerEvents.SentrypeerEvent
 
   def user_agents_highest_top_10 do
-    query = """
-    SELECT sip_user_agent, MAX(event_timestamp) AS seen_last,
-    COUNT(sip_user_agent) AS seen_total
-    FROM sentrypeerevents
-    GROUP BY sip_user_agent
-    ORDER BY seen_total
-    DESC limit 10;
-    """
+#    query = """
+#    SELECT sip_user_agent, MAX(event_timestamp) AS seen_last,
+#    COUNT(sip_user_agent) AS seen_total
+#    FROM sentrypeerevents
+#    GROUP BY sip_user_agent
+#    ORDER BY seen_total
+#    DESC limit 10;
+#    """
+#
+#    Repo.query!(query)
 
-    Repo.query!(query)
+     query =
+      from s in SentrypeerEvent,
+        group_by: s.sip_user_agent,
+        order_by: [desc: count(s.sip_user_agent)],
+        limit: 10,
+        select: %{
+          sip_user_agent: s.sip_user_agent,
+          seen_last: max(s.event_timestamp),
+          seen_total: count(s.sip_user_agent)
+        }
+
+    Repo.all(query)
   end
 
   def countries_of_origination_top_10 do
   end
 
   def sip_methods_top_10 do
-    query = """
-    SELECT sip_method, COUNT(sip_method) AS seen_total
-    FROM sentrypeerevents
-    GROUP BY sip_method
-    ORDER BY seen_total
-    DESC limit 10;
-    """
+    #    query = """
+    #    SELECT sip_method, COUNT(sip_method) AS seen_total
+    #    FROM sentrypeerevents
+    #    GROUP BY sip_method
+    #    ORDER BY seen_total
+    #    DESC limit 10;
+    #    """
+    #
+    #    Repo.query!(query)
 
-    Repo.query!(query)
+    query =
+      from s in SentrypeerEvent,
+        group_by: s.sip_method,
+        order_by: [desc: count(s.sip_method)],
+        limit: 10,
+        select: %{
+          sip_method: s.sip_method,
+          seen_total: count(s.sip_method)
+        }
+
+    Repo.all(query)
   end
 
   def events_per_day_total do
-    query = """
-    SELECT date_trunc('day', event_timestamp) AS day,
-    COUNT(event_timestamp) as seen_total
-    FROM sentrypeerevents
-    GROUP BY day
-    ORDER BY day;
-    """
+    #    query = """
+    #    SELECT date_trunc('day', event_timestamp) AS day,
+    #    COUNT(event_timestamp) as seen_total
+    #    FROM sentrypeerevents
+    #    GROUP BY day
+    #    ORDER BY day;
+    #    """
+    #
+    #    Repo.query!(query)
 
-    Repo.query!(query)
+    query =
+      from s in SentrypeerEvent,
+        group_by: fragment("date_trunc('day', event_timestamp)"),
+        order_by: [asc: fragment("date_trunc('day', event_timestamp)")],
+        select: %{
+          day: fragment("date_trunc('day', event_timestamp)"),
+          seen_total: count(s.event_timestamp)
+        }
+
+    Repo.all(query)
   end
 end
